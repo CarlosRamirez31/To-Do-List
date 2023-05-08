5c03d783adc3e0be7fd45bfe8b52a6152d8a0385
CarlosRamirez31 <cr2503150@gmail.com>
2023-05-08 16:55:49 -0400

    New event to modify the behavior of the lists

--- js/app.js
+++ js/app.js
@@ -2,15 +2,23 @@ const fecha = document.getElementById("fecha");
 const input = document.getElementById("texto");
 const botonEnter = document.getElementById("enter");
 const lista = document.getElementById("lista");
-const uncheck = "bi bi-circle";
-const check = "bi bi-check2-circle";
+const uncheck = "bi-circle";
+const check = "bi-check-circle";
+const lineThrough = "line-through";
+const FECHA = new Date();
 
+fecha.innerHTML = FECHA.toLocaleDateString("es-ES", {weekday: "long", month: "short", day: "numeric"});
 
-function agregarTarea(texto){
-    const elemento = `<li>
-                        <i class="bi bi-circle" data="realizado" id="0"></i>
-                        <p class="texto">${texto}</p>
-                        <i class="bi bi-trash-fill" data="eliminado" id="0"></i>
+function agregarTarea(texto, realizado, finalizado){
+    if(finalizado) return;
+
+    const REALIZADO = realizado? check: uncheck; 
+    const LINE = realizado? lineThrough: ""
+
+    const elemento = `<li id="elemento">
+                        <i class="bi ${REALIZADO}" data="realizado"></i>
+                        <p class="texto ${LINE}">${texto}</p>
+                        <i class="bi bi-trash-fill" data="eliminado"></i>
                       </li>
                       `
     lista.insertAdjacentHTML('beforeend', elemento);
@@ -18,18 +26,35 @@ function agregarTarea(texto){
 
 function agregarTexto(){
     const tarea = input.value;
-    console.log(tarea);
 
     if(tarea)
-        agregarTarea(tarea);
+        agregarTarea(tarea, false, false);
 
     input.value = "";
 }
 
 botonEnter.addEventListener("click", agregarTexto);
 
-
 document.addEventListener("keyup", function(event){
     if(event.key == "Enter")
         agregarTexto();
+});
+
+function tareaRealizada(elemento){
+    elemento.classList.toggle(check)
+    elemento.classList.toggle(uncheck);
+    elemento.parentNode.querySelector(".texto").classList.toggle(lineThrough)
+}
+
+function tareaEliminada(elemento){
+    elemento.parentNode.parentNode.removeChild(elemento.parentNode)
+}
+
+
+lista.addEventListener("click", function(event){
+    const elemento = event.target;
+    const data = elemento.attributes.data.value;
+
+    if(data === "realizado") tareaRealizada(elemento);
+    if(data === "eliminado") tareaEliminada(elemento);
 });
